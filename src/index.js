921c168ef4fdd341d6aec15ef8aceb814ee6985f
dmenliyakhmatov <51416462+dmenliyakhmatov@users.noreply.github.com>
2020-03-09 23:49:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrString = str.split('');
+  
+  for (let i=0; i<arrString.length; i++) {
+    for (let j=0; j<bracketsConfig.length; j++) {
+      if (arrString[i] == bracketsConfig[j][0] && arrString[i+1] == bracketsConfig[j][1] ) {
+        arrString.splice(i,2);
+        i = 0;
+      }
+    }
+  }
+  
+  if (arrString.length ==2) {
+    for (let j=0; j<bracketsConfig.length; j++) {
+      if (arrString[0] == bracketsConfig[j][0] && arrString[1] == bracketsConfig[j][1] ) {
+        arrString.splice(0,2);
+      }
+    }
+  }
+
+  if (arrString.length == 0) return true;
+  
+  return false
 }
